0cda70762e8811f4f40d6d8d2519fdd77b3fcfde
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-04-21 15:34:50 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var wakeDog = function(dogName = "Ozzie", dogBreed = "Mutt"){
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`
+}
+
+var leashDog = function(dogName = "Ozzie", dogBreed = "Mutt"){
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`
+}
+
+var walkToPark = function(dogName="Ozzie", dogBreed="Mutt") {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+  }
+  
+var throwFrisbee = function(dogName="Ozzie", dogBreed="Mutt") {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+  }
+  
+var walkHome = function(dogName="Ozzie", dogBreed="Mutt") {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`
+  }
+  
+var unleashDog = function(dogName="Ozzie", dogBreed="Mutt") {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`
+  }
+
+ const routine = [wakeDog, leashDog, walkToPark,throwFrisbee,walkHome, unleashDog]
+
+ function exerciseDog(dog, breed){
+    return routine.map(fn => fn(dog, breed))
+ }
+ 
